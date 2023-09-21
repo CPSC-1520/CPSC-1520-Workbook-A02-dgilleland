f6107fd1072d04e4d050939a15fab2150dc06783
Dan Gilleland <dgilleland@nait.ca>
2023-09-21 09:20:51 -0600

    Demo re-using a function and other events with inline functions

--- src/008/demo-events/js/main.js
+++ src/008/demo-events/js/main.js
@@ -2,7 +2,7 @@
 var featureImage = document.querySelector('img.feature');
 
 // 2. create the event listener function
-function handleFeatureClick() {
+const handleFeatureClick = function () {
     console.log('img.feature was clicked...');
     var desc = document.querySelector('.feature.description');
     desc.classList.remove('hidden');
@@ -10,3 +10,14 @@ function handleFeatureClick() {
 
 // 3. add event listener
 featureImage.addEventListener('click', handleFeatureClick);
+
+// 4. use the same event handler (handleFeatureClick) for a listener on another element
+const headerEl = document.querySelector('header > h1');
+headerEl.addEventListener('click', handleFeatureClick);
+
+// 5. Let's use a double-click on the header element to hide the description.
+//    This time, instead of passing in a function for the event handler, we'll pass in an anonymous function.
+headerEl.addEventListener('dblclick', function () {
+    var desc = document.querySelector('.feature.description');
+    desc.classList.add('hidden');
+});
