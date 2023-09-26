56a3d68a2bb3c876980302fd981cba95b103b8d3
Dan Gilleland <dgilleland@nait.ca>
2023-09-26 14:44:40 -0600

    Notes on the HTML DOM Tree

--- src/012/demo-thumbnail_resize/js/main.js
+++ src/012/demo-thumbnail_resize/js/main.js
@@ -1,5 +1,17 @@
-// When a gallery thumbnail size button is clicked, the corresponding size class
-// should be added to the div.thumbnails element.  This will apply the corresponding
-// CSS rule to  modify the display the of the thumbnail images.  In addition to this,
-// the clicked button should be made active (i.e. add the active class).
+// When a gallery thumbnail size button is clicked,
+// the corresponding size class should be added to the
+// div.thumbnails element. This will apply the corresponding
+// CSS rule to  modify the display the of the thumbnail
+// images. In addition to this, the clicked button should
+// be made active (i.e. add the active class).
 
+/* HTML DOM Tree
+main
+    nav
+        ul.thumbnail-sizes
+         x3=li.thumbnails-XX [.active]
+    section.gallery
+        h1
+        div.thumbnails.lg
+            img[src="images/PLACE_thumb.png"][alt="PLACE"]
+*/
