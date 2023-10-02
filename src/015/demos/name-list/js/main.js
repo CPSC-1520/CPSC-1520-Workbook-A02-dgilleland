4fa214602e5532511a11a584fffd91cad84ccd0f
Dan Gilleland <dgilleland@nait.ca>
2023-10-02 10:00:39 -0600

    Finish intro to loops

--- src/015/demos/name-list/js/main.js
+++ src/015/demos/name-list/js/main.js
@@ -1,20 +1,32 @@
 /*
-	TASK: prompt for individual names and add them to 
-		  a list.  Confirm after each name is entered
-		  whether the user would like to enter another
-		  name or not.
+  TASK: prompt for individual names and add them to 
+      a list.  Confirm after each name is entered
+      whether the user would like to enter another
+      name or not.
 */
-let outDiv = document.querySelector('div');
+// One way to "pull" items out of the global scope
+// is to wrap our code inside of a function
+window.onload = function() {
+  // Now, my local variables are no longer
+  // in the global scope, but are restricted
+  // to this function
+  let outDiv = document.querySelector('div');
 
-let myHTML = '<ul>';
+  let myHTML = '<ul>';
 
-// Add my names
-do {
+  // Add my names
+  do {
     let aName = prompt('Enter name: ');
-    myHTML += '<li>' + aName + '</li>';
-} while(confirm('Another name?'));
+    // Template literals start and end with a backtick
+    myHTML += `<li>${aName}</li>`;
+    //             \______/
+    //               |-  Placeholder
+  } while(confirm('Another name?'));
+  //      \______________________/
+  //         |-   true if OK, otherwise false
 
-myHTML += '</ul>';
+  myHTML += '</ul>';
 
-// display the list
-outDiv.innerHTML = myHTML;
+  // display the list
+  outDiv.innerHTML = myHTML;
+}
