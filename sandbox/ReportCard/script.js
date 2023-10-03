719f3d2b7cd9b9bd3feaa879f25cbf5a7e1ae22d
Dan Gilleland <dgilleland@nait.ca>
2023-10-03 16:54:29 -0600

    Plan out some ideas for the ReportCard project

---
+++ sandbox/ReportCard/script.js
@@ -0,0 +1,11 @@
+// TODO: Outline the tasks/logic using only comments
+
+// Purpose: This presents a form to input the course number and name of the course as well as the final mark for the course.
+
+/* AddCourse button click
+- Get the user input (course number & name)
+  - querySelector('#courseNumber')
+  - querySelector('#courseName')
+- Add that to my HTML table as a row of data
+
+*/
