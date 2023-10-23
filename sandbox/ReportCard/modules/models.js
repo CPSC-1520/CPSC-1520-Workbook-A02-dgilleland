cb4abeefaea15a73e78920939a4d66d0e55f0a64
Dan Gilleland <dgilleland@nait.ca>
2023-10-23 09:24:36 -0600

    Start building the ReportCard code

---
+++ sandbox/ReportCard/modules/models.js
@@ -0,0 +1,8 @@
+// modules/models.js
+// This file contains the models for the application
+
+export const Course = function(name, number, grade) {
+    this.name = name;
+    this.number = number;
+    this.grade = grade;
+}
--- sandbox/ReportCard/script.js
+++ sandbox/ReportCard/script.js
@@ -1,11 +1,19 @@
 // TODO: Outline the tasks/logic using only comments
 
 // Purpose: This presents a form to input the course number and name of the course as well as the final mark for the course.
+import { Course } from './modules/models.js';
 
 /* AddCourse button click
 - Get the user input (course number & name)
   - querySelector('#courseNumber')
   - querySelector('#courseName')
 - Add that to my HTML table as a row of data
-
 */
+const btn = document.querySelector('button');
+console.log(btn);
+btn.addEventListener('click', () => {
+    const courseNumber = document.querySelector('#courseNumber').value;
+    const courseName = document.querySelector('#courseName').value;
+    const course = new Course(courseName, courseNumber);
+    console.log(course.name, course.number, course.grade);
+});
