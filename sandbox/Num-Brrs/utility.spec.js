b6f645dd0fa6f9cb99d5f01220839d9bee1b696d
Dan Gilleland <dgilleland@nait.ca>
2023-10-26 11:18:34 -0600

    Simple Node App
    Demos the use of JSON for configuration (package.json)

---
+++ sandbox/Num-Brrs/utility.spec.js
@@ -0,0 +1,8 @@
+// utility.spec.js
+import {it, expect} from 'vitest';
+
+it('should be green', () => { }); // TODO: remove
+it('should be a good demo', () => {
+    // This is the body of the test
+    expect(theDemo).toBe('Working');
+});
