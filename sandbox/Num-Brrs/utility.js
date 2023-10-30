38a5212d8245a3ec411d895ebea618e9f1c570a2
Dan Gilleland <dgilleland@nait.ca>
2023-10-30 09:38:07 -0600

    Finished (most of) the tests

--- sandbox/Num-Brrs/utility.js
+++ sandbox/Num-Brrs/utility.js
@@ -1,12 +1,25 @@
 export const calculate = function(operator, values) {
+    // Guard Clause - Validate my input
+    if(operator !== '+' && 
+       operator !== '-' && 
+       operator !== '*' && 
+       operator !== '/')
+        throw new Error(`'${operator}' is not a valid math operation`);
+    if(values === null || values === undefined || values.constructor.name !== 'Array')
+        throw new Error(`'${values}' is not an array of numbers`)
+    
+    // If it gets this far, then I have good data
+    // to work with
     let result = values[0];
     for(let index = 1; index < values.length; index++) {
         if(operator === '+') {
             result += values[index];
         } else if(operator === '-') {
             result = result - values[index];
-        } else {
+        } else if(operator === '*') {
             result = result * values[index];
+        } else if(operator === '/') {
+            result = result / values[index];
         }
     }
     return result;
--- sandbox/Num-Brrs/utility.spec.js
+++ sandbox/Num-Brrs/utility.spec.js
@@ -18,10 +18,39 @@ it('should multiply three numbers', () => {
     const actual = calculate('*', [4, 2, 3]);
     expect(actual).toBe(24);
 });
-it.todo('should multiple two numbers', () => {
+it('should multiple two numbers', () => {
     const actual = calculate('*', [4, 5]);
     expect(actual).toBe(20);
 });
-it.todo('should divide two numbers', () => {
-    expect(details).toBe('correct');
-})
+it('should divide two numbers', () => {
+    const actual = calculate('/', [1, 2, 5]);
+    expect(actual).toBe(0.1);
+});
+// Unhappy path tests
+it('should reject invalid operator', () => {
+    // Arrange - Creating a function that can be
+    //           passed into my expect() method
+    const act = () => calculate('bob', [4, 5, 6]);
+    // Assert
+    expect(act).toThrowError(`'bob' is not a valid math operation`);
+});
+
+it('should reject invalid array', () => {
+    const act = () => calculate('+', 'bob');
+    expect(act).toThrowError(`'bob' is not an array of numbers`);
+});
+
+it('should reject null for the array', () => {
+    const act = () => calculate('+', null);
+    expect(act).toThrowError(`'null' is not an array of numbers`);
+});
+
+it('should reject the array unless it has all numbers', () => {
+    const act = () => calculate('+', [1, 3, 5, null]);
+    expect(act).toThrowError('the array must only contain numbers');
+});
+
+it.todo('should reject an empty array', () => {
+    const act = () => calculate('+', []);
+    expect(act).toThrowError('the array must have at least one number');
+});
