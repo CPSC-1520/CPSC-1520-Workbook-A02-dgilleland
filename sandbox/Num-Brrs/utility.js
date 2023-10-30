3d46456565332aff01d8b03116999c0114e964db
Dan Gilleland <dgilleland@nait.ca>
2023-10-30 08:50:53 -0600

    Refactor my code

--- sandbox/Num-Brrs/utility.js
+++ sandbox/Num-Brrs/utility.js
@@ -1,18 +1,13 @@
 export const calculate = function(operator, values) {
-    let result;
-    if(operator === '+') {
-        result = values[0];
-        for(let index = 1; index < values.length; index++) {
-                result += values[index];
-        }
-    } else if(operator === '-') {
-        result = values[0];
-        for(let index = 1; index < values.length; index++)
+    let result = values[0];
+    for(let index = 1; index < values.length; index++) {
+        if(operator === '+') {
+            result += values[index];
+        } else if(operator === '-') {
             result = result - values[index];
-    } else {
-        result = values[0];
-        for(let index = 1; index < values.length; index++)
+        } else {
             result = result * values[index];
+        }
     }
     return result;
 }
