3906cf7305a58b8179a9f7d0874501c81536f685
Dan Gilleland <dgilleland@nait.ca>
2023-11-20 09:12:16 -0700

    WIP - Node console app
    The actual files that were renamed (previous commit was only the delete of the files, not an actual rename), plus a new index.js to replace the previous one

---
+++ sandbox/NodeConsoleApp/lib/date-functions.js
@@ -0,0 +1,3 @@
+export const epochToDate = function(epochNumber) {
+    return new Date(epochNumber * 1000);
+}
---
+++ sandbox/NodeConsoleApp/lib/index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+// The line above is needed for this file to be executable as a script.
+// It specifies that the script should be run using the node interpreter.
+// It is also possible to run this script using the following command:
+//      node ./sandbox/NodeConsoleApp/lib/index.js
+// (assuming you are running from the console at the root of this repository)
+
+// This is the "entry point" for the application.
+console.log('Hello, world!');
+/* How Node Works
+    When Node runs, it will have a global variable called `process` that contains information about the whole "context" of the application that has been started.
+    One of the properties of `process` is `argv`, which is an array of strings representing the command line arguments that were passed to the application.
+    The first element in the array is the path to the Node executable.
+    The second element is the path to the script that is being run.
+    The remaining elements are the arguments that were explicitly passed to the script.
+    For example, if you run the following command:
+         node ./sandbox/NodeConsoleApp/lib/index.js --foo bar baz
+    Then `process.argv` will be:
+         [
+             '/usr/local/bin/node',
+             '/Users/username/Projects/JavaScriptSandbox/sandbox/NodeConsoleApp/lib/index.js',
+             '--foo',
+             'bar',
+             'baz'
+         ]
+*/
+// The following code will print the command line arguments that were passed to the script.
+console.log('Command line arguments:');
+console.log(process.argv);
+// The following code will print the current working directory.
+console.log('Current working directory:');
+console.log(process.cwd());
+// The following code will print the environment variables.
+console.log('Environment variables:');
+console.log(process.env);
